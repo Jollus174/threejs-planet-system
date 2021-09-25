1312bc51efa4c2b90df68702357aad5f27f35357
Jollus174 <joel@joeldoesdigital.com>
2021-09-25 17:04:34 +1000

    prog - adding torus wireframe mesh + rotation

--- threejs/main.js
+++ threejs/main.js
@@ -1,6 +1,44 @@
-import './style.css'
+/* globals requestAnimationFrame */
+'use strict';
+import './style.css';
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+const renderTarget = document.querySelector('#bg');
+
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+	canvas: renderTarget
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.z = 30;
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const animate = () => {
+	requestAnimationFrame(animate);
+
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.01;
+	torus.rotation.z += 0.01;
+
+	// render == DRAW
+	renderer.render(scene, camera);
+};
+
+animate();
+
+/* window.addEventListener('resize', () => {
+	console.log('resizing!');
+	// renderer.setSize(window.innerWidth, window.innerHeight);
+
+	// TODO: possibly add a debounce
+}); */
