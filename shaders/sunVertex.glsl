57116e50ec908bdf7385beb00422610c0283d4bf
Jollus174 <joel@joeldoesdigital.com>
2021-10-03 20:23:26 +1100

    rainbow sphere sun shader

--- shaders/sunVertex.glsl
+++ shaders/sunVertex.glsl
@@ -1,8 +1,11 @@
 varying vec2 vUv;
 varying vec3 vertexNormal;
+varying vec3 vPosition;
+
 
 void main() {
   vUv = uv;
+  vPosition = position;
   // vertexNormal = normal;
   vertexNormal = normalize(normalMatrix * normal);
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
