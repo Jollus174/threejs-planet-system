a489ed6d9bc977e86ed14f8287af41f5d3a395de
Jollus174 <joel@joeldoesdigital.com>
2021-10-03 17:09:54 +1100

    adding celestial bodies to individual 3D objects for better grouping

--- modules/loadManager.js
+++ modules/loadManager.js
@@ -5,17 +5,17 @@ const loadManager = () => {
 	const manager = new LoadingManager();
 	const loadingProgressBar = document.querySelector('#bar');
 	manager.onStart = (url, itemsLoaded, itemsTotal) => {
-		console.log(`Started loading file: ${url}.\nLoaded ${itemsLoaded} of ${itemsTotal} files.`);
+		// console.log(`Started loading file: ${url}.\nLoaded ${itemsLoaded} of ${itemsTotal} files.`);
 	};
 
 	manager.onLoad = () => {
-		console.log('Loading complete!');
-		document.querySelector('body').classList.remove('loading');
-		document.querySelector('body').classList.add('loaded');
+		// console.log('Loading complete!');
+		// document.querySelector('body').classList.remove('loading');
+		// document.querySelector('body').classList.add('loaded');
 	};
 
 	manager.onProgress = (url, itemsLoaded, itemsTotal) => {
-		console.log(`Started loading file: ${url}.\nLoaded ${itemsLoaded} of ${itemsTotal} files.`);
+		// console.log(`Started loading file: ${url}.\nLoaded ${itemsLoaded} of ${itemsTotal} files.`);
 		loadingProgressBar.style.setProperty('--progress-amount', `${(itemsLoaded / itemsTotal) * 100}%`);
 	};
 
