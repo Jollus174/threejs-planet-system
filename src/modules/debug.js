0da20eb49bc16ba969bb4180b624ff0b5d4a0c81
Joel Sweetman <joel@joeldoesdigital.com>
2023-01-05 20:40:28 +1100

    easier for debug to fire between a basic and shader material now

--- src/modules/debug.js
+++ src/modules/debug.js
@@ -6,16 +6,19 @@ import fragmentShader from './shaders/debug/simpleFragmentShader.glsl';
 
 const createBasicCube = () => {
 	const geometry = new THREE.BoxGeometry(100000000, 100000000, 100000000, 10, 10);
-	const material = new THREE.ShaderMaterial({
-		uniforms: {
-			colorA: { type: 'vec3', value: new THREE.Color(0xff0000) },
-			colorB: { type: 'vec3', value: new THREE.Color(0x0000ff) }
-		},
-		vertexShader,
-		fragmentShader
-	});
+	const materials = {
+		shaderMaterial: new THREE.ShaderMaterial({
+			uniforms: {
+				colorA: { type: 'vec3', value: new THREE.Color(0xff0000) },
+				colorB: { type: 'vec3', value: new THREE.Color(0x0000ff) }
+			},
+			vertexShader,
+			fragmentShader
+		}),
+		basicMaterial: new THREE.MeshBasicMaterial()
+	};
 
-	const boxMesh = new THREE.Mesh(geometry, material);
+	const boxMesh = new THREE.Mesh(geometry, materials.shaderMaterial);
 	orrery.scene.add(boxMesh);
 };
 
